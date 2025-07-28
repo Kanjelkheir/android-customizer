823d1440146f22c442e264d1095611d573d60316
Elie Khalil <eliekh05@gmail.com>
2025-07-28 10:03:25 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,5 @@ const app = new App({
   target: document.body,
 });
 
-// Register the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/public/pwabuilder-sw.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(err => {
-        console.error('Service Worker registration failed:', err);
-      });
-  });
-}
 
 export default app;
