046b82ede8df7dfffccfbbe419d5c2a68ac2ff13
Elie Khalil <eliekh05@gmail.com>
2025-07-29 14:40:13 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const app = new App({
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/public/sw.js')
       .then(reg => console.log('Service worker registered:', reg))
       .catch(err => console.error('Service worker registration failed:', err));
   });
